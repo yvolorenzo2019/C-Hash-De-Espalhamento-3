d2ae582aa746f34cc826653afc2d07c85fe135f6
Yvo Lorenzo <47569750+yvolorenzo2019@users.noreply.github.com>
2019-10-18 14:32:16 -0300

    First

---
+++ Atividade_Hash_C#.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace md5hash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string professores;
+            string result;
+            professores = "Graciete e Jussimar são Legais <3";
+
+            //Criando o Objeto com  a Hash de Espalhamento MD5
+            MD5 md5hash = MD5.Create();
+
+            //Guardando a variavel do Hash
+            byte[] data = md5hash.ComputeHash(Encoding.UTF8.GetBytes(professores));
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sb.Append(data[i].ToString("x2"));
+            }
+
+            result = sb.ToString();
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+    }
+}
